ba9ba108f32b224954ccd68ba838a8b249f74061
Huascar <huascar.sanchez@gmail.com>
2023-02-17 10:55:03 -0800

    Refactors RandoopMojo's main APIs

--- src/main/java/randoop/maven/RandoopMojo.java
+++ src/main/java/randoop/maven/RandoopMojo.java
@@ -1,17 +1,13 @@
 package randoop.maven;
 
 import com.google.common.base.Preconditions;
-import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
-import com.google.common.reflect.ClassPath;
-import com.google.common.reflect.ClassPath.ClassInfo;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.ProcessBuilder.Redirect;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -20,13 +16,13 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import org.apache.maven.artifact.Artifact;
@@ -37,32 +33,45 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import randoop.maven.utils.Util;
 
+/**
+ * Entry point to the Randoop maven plugin; more specifically to
+ * the 'gentests' goal.
+ *
+ * Before execution, the plugin checks if tests have been run and
+ * there are any surefire reports. If there are any, the plugin
+ * will persist them, re-run Randoop, and then let surefire run the
+ * regenerated tests. The last step will re-generate a new surefire report,
+ * which the Soundness test verifier will pick up and compare it against
+ * the previously generated.
+ *
+ */
+@SuppressWarnings("unused")
 @Mojo(name = "gentests", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
 public class RandoopMojo extends AbstractMojo {
 
   @Parameter(required = true) private String packageName;
   @Parameter(required = true, defaultValue = "${project.build.outputDirectory}")
   private String sourceDirectory;
-
-  // ${project.basedir}/src/test/java
   @Parameter(required = true, defaultValue = "${project.build.directory}/generated-test-sources/java")
   private String targetDirectory;
   @Parameter(required = true, defaultValue = "60") private int timeoutInSeconds;
-  @Parameter(defaultValue = "false") String permitNonZeroExitStatus;
-
-  @Parameter(defaultValue = "etb2") String rootJavaPackage;
+  @Parameter(defaultValue = "false") private boolean permitNonZeroExitStatus;
+  @Parameter(defaultValue = "etb2") private String rootJavaPackage;
+  @Parameter(defaultValue = "${project.build.directory}/surefire-reports/")
+  private String surefireReportsDir;
 
   @Parameter( defaultValue = "${project}", readonly = true)
   private MavenProject project;
 
   @Override public void execute() throws MojoExecutionException, MojoFailureException {
-    // Resolve any dependencies to the Randoop tool
-    final List<URL> urls = new LinkedList<>(resolveProjectClasses());
-    urls.addAll(resolveProjectDependencies(project));
-    // Add randoop plugin Jar and randoop dependency here
-    urls.addAll(resolvePluginJarWithRandoop());
+    // Check if surefire reports have been generated. If they have,
+    // copy them to ${project.basedir}/.surefire.d
+    copySurefireReportsIfExist();
 
+    // Resolve any dependencies to the Randoop tool
+    final List<URL> urls = resolveRandoopDependencies();
     final List<String> args = buildArgs(urls);
 
     // Build Randoop command line
@@ -79,14 +88,50 @@ public class RandoopMojo extends AbstractMojo {
           "Test generation failure. Process exited with code " + exitCode);
     }
 
-    getLog().info((exitCode == 0 ? "Randoop finished." : "Randoop did not finish."));
+    getLog().info("Randoop " + (exitCode == 0 ? "finished." : "did not finish."));
 
-    // TODO(has) prevent Randoop from creating empty directories for packageName's directories;
-    // For some unknown reason, Randoop keeps creating these empty directories; starting with the
-    // project name as the root.
-    // Temporary fix: Delete infamous directories after executing Randoop.
+    // TODO(has) Prevent Randoop from generating empty directories matching the packageName
+    // Temp. fix: search for those empty directories and delete them if found
+    trashRandoopLeftovers();
+  }
+
+  private List<URL> resolveRandoopDependencies() throws MojoExecutionException {
+    final List<URL> urls = new LinkedList<>(resolveProjectClasses());
+    urls.addAll(resolveProjectDependencies(project));
+    // Add randoop plugin Jar and randoop dependency here
+    urls.addAll(resolvePluginJarWithRandoop());
+    return urls;
+  }
+
+  private void copySurefireReportsIfExist() throws MojoFailureException {
+    final Predicate<File> isRandoopSuite = f -> f.getName().equalsIgnoreCase(
+        packageName  + ".RegressionTest.txt")
+        || f.getName().equalsIgnoreCase(
+            packageName + ".ErrorTest.txt");
+
+    final Path surefirePath = Paths.get(surefireReportsDir);
+    if (Files.exists(surefirePath)){
+      Set<Path> matchedFiles = Util.findFiles(surefirePath, Util.TEXT_MATCHER).stream()
+          .map(Path::toFile)
+          .filter(isRandoopSuite)
+          .map(File::toPath)
+          .collect(ImmutableSet.toImmutableSet());
+
+      final Path baseDir = project.getBasedir().toPath();
+      for (Path each : matchedFiles){
+        try {
+          Util.copyFiles(baseDir, baseDir.resolve(".surefire.d"), matchedFiles);
+        } catch (IOException e) {
+          throw new MojoFailureException("Unable to copy surefire reports!", e);
+        }
+      }
+    }
+  }
+
+  private void trashRandoopLeftovers() {
     Path pathToBeDeleted = project.getBasedir().toPath().resolve(rootJavaPackage);
-    if (!Files.exists(pathToBeDeleted)) return;
+    if (!Files.exists(pathToBeDeleted))
+      return;
     try (Stream<Path> walk = Files.walk(pathToBeDeleted)) {
       walk.sorted(Comparator.reverseOrder())
           .forEach(Util::deleteDir);
@@ -116,7 +161,7 @@ public class RandoopMojo extends AbstractMojo {
     args.add("--junit-output-dir=" + targetDirectory);
 
     // Add project classes
-    try(URLClassLoader classLoader = new URLClassLoader(toURLArray(urls))) {
+    try(URLClassLoader classLoader = new URLClassLoader(Util.toURLArray(urls))) {
       final Set<Class<?>> inPackageClasses = Util.classesLookup(packageName, classLoader);
       for (Class<?> eachClass : inPackageClasses){
         getLog().info("Add class " + eachClass.getName());
@@ -146,7 +191,7 @@ public class RandoopMojo extends AbstractMojo {
       process.waitFor(timeoutInSeconds + 3, TimeUnit.SECONDS);
 
       exitCode = process.exitValue();
-      if (exitCode != 0 && !Boolean.parseBoolean(permitNonZeroExitStatus)){
+      if (exitCode != 0 && !permitNonZeroExitStatus){
         throw new IOException("Randoop process returned " + exitCode);
       }
     }
@@ -155,30 +200,6 @@ public class RandoopMojo extends AbstractMojo {
 
   }
 
-
-  private int newCallToRandoop(final List<String> args) throws IOException {
-    final ProcessBuilder externalCall = new ProcessBuilder(args);
-    externalCall.redirectErrorStream(true);
-    externalCall.redirectOutput(Redirect.PIPE);
-    externalCall.directory(project.getBasedir());
-    getLog().info("Randoop started with time limit of " + timeoutInSeconds + " seconds.");
-
-    final Process randoop = externalCall.start();
-    try {
-      randoop.waitFor(timeoutInSeconds + 3, TimeUnit.SECONDS);
-    } catch (InterruptedException e){
-      throw new IOException(e);
-    }
-
-    int exitCode = randoop.exitValue();
-    if (randoop.exitValue() != 0){
-      getLog().info("Exit code " + exitCode);
-//      throw new IOException("Randoop process returned " + exitValue);
-    }
-
-    return exitCode;
-
-  }
 
   private Set<URL> resolvePluginJarWithRandoop(){
     final List<URL> pluginJarUrls = new ArrayList<>();
@@ -195,7 +216,7 @@ public class RandoopMojo extends AbstractMojo {
     final Optional<Path> randoopJar = Util.findRandoopJar(pluginJar.getFile(), randoopVersion);
     if (randoopJar.isPresent() && Files.exists(randoopJar.get())){
       try {
-        pluginJarUrls.add(newURL(randoopJar.get().toString()));
+        pluginJarUrls.add(Util.newURL(randoopJar.get().toString()));
       } catch (MalformedURLException e) {
         getLog().warn("Unable to find Randoop Jar", e);
       }
@@ -225,7 +246,7 @@ public class RandoopMojo extends AbstractMojo {
   private List<URL> resolveProjectClasses() throws MojoExecutionException {
     final URL classesLocation;
     try {
-      classesLocation = newURL(sourceDirectory);
+      classesLocation = Util.newURL(sourceDirectory);
     } catch (MalformedURLException e){
       throw new MojoExecutionException("Could not create source path!", e);
     }
@@ -233,75 +254,4 @@ public class RandoopMojo extends AbstractMojo {
     return ImmutableList.of(classesLocation);
   }
 
-  private static URL newURL(final String filepath) throws MalformedURLException {
-    return Paths.get(filepath).toUri().toURL();
-  }
-
-  private static URL[] toURLArray(final Collection<URL> candidateURLs){
-    return candidateURLs.toArray(new URL[0]);
-  }
-
-  static class Util {
-    static Optional<Path> findRandoopJar(String seedPath, String randoopVersion){
-      Optional<Path> parentDir = findParentDir(seedPath, "repository");
-
-      Path repository;
-      if (!parentDir.isPresent()){
-        final Path orElse = Paths.get(
-            System.getProperty("user.home"),
-            ".m2",
-            "repository");
-
-        if (!Files.exists(orElse)) return Optional.empty();
-        repository = orElse;
-      } else {
-        repository = parentDir.get();
-      }
-
-      final Path randoopJar = repository.resolve("randoop")
-          .resolve("randoop-all")
-          .resolve(randoopVersion)
-          .resolve("randoop-all-" + randoopVersion +".jar");
-
-      if (!Files.exists(randoopJar)) return Optional.empty();
-
-      return Optional.of(randoopJar);
-    }
-
-    static Optional<Path> findParentDir(String filepath, String targetName){
-      // Only valid dir names allowed
-      Preconditions.checkArgument(!Strings.isNullOrEmpty(targetName));
-
-      Path current = Paths.get(filepath);
-      if (!Files.exists(current)) return Optional.empty();
-
-      while(current != null && !current.getFileName().toString().equals(targetName)){
-        current = current.getParent();
-      }
-
-      return Optional.ofNullable(current);
-    }
-
-    static Set<Class<?>> classesLookup(final String packageName, ClassLoader loader) throws IOException {
-      Preconditions.checkNotNull(packageName);
-
-      final ClassLoader lenientClassLoader = Optional
-          .ofNullable(loader)
-          .orElse(ClassLoader.getSystemClassLoader());
-
-      return ClassPath.from(lenientClassLoader)
-          .getAllClasses()
-          .stream()
-          .filter(cls -> cls.getPackageName().equalsIgnoreCase(packageName))
-          .map(ClassInfo::load)
-          .collect(Collectors.toSet());
-    }
-
-
-    static void deleteDir(Path path){
-      try {
-        Files.delete(path);
-      } catch (IOException ignored){}
-    }
-  }
 }
---
+++ src/main/java/randoop/maven/utils/Util.java
@@ -0,0 +1,228 @@
+package randoop.maven.utils;
+
+import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.reflect.ClassPath;
+import com.google.common.reflect.ClassPath.ClassInfo;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Util {
+
+  public static final PathMatcher JAVA_MATCHER = FileSystems.getDefault().getPathMatcher("glob:*.java");
+  public static final PathMatcher TEXT_MATCHER = FileSystems.getDefault().getPathMatcher("glob:*.txt");
+
+  // Regular expression which matches expected names of JUnit test classes.
+  // thx to https://github.com/sevntu-checkstyle/sevntu.checkstyle
+  public static final Pattern RANDOOP_TEST_NAME_REGEX = Pattern.compile(
+      "RegressionTest\\d*|RegressionTests\\d*|RegressionTest.+|RegressionTests.+|ErrorTest\\d*|ErrorTests\\d*|ErrorTest.+|ErrorTests.+");
+
+
+  private Util(){
+    throw new Error("Cannot be instantiated!");
+  }
+
+  public static Path copyFiles(Path src, Path tgt, Collection<Path> files) throws IOException {
+    Preconditions.checkArgument(Files.exists(src));
+    Preconditions.checkNotNull(tgt);
+
+    if (!Files.exists(tgt)){
+      Files.createDirectory(tgt);
+    }
+
+    assert Files.exists(tgt);
+
+    for (Path each : files) {
+      try {
+        Files.copy(
+            src,
+            tgt.resolve(each.getFileName()),
+            StandardCopyOption.REPLACE_EXISTING);
+      } catch (IOException e) {
+        // failed to copy files
+        throw new RuntimeException(e.getMessage(), e);
+      }
+    }
+
+    return tgt;
+  }
+
+  public static Path copyTree(Path src, Path tgt) throws IOException {
+    Preconditions.checkArgument(Files.exists(src));
+    Preconditions.checkNotNull(tgt);
+
+    if (!Files.exists(tgt)){
+      Files.createDirectory(tgt);
+    }
+
+    assert Files.exists(tgt);
+
+    try (Stream<Path> stream = Files.walk(src)) {
+      stream.forEach(f -> {
+        try {
+          Files.copy(src, tgt.resolve(f.getFileName()),
+              StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException e) {
+          // failed to copy tree
+          throw new RuntimeException(e.getMessage(), e);
+        }
+      });
+    }
+
+    return tgt;
+  }
+
+  public static Optional<Path> createTempDir(String prefix) {
+    try {
+      prefix = Strings.isNullOrEmpty(prefix) ? "RandoopTempDir" : prefix;
+      return Optional.of(Files.createTempDirectory(prefix));
+    } catch (IOException ignored) {
+    }
+    return Optional.empty();
+  }
+
+  public static Set<Path> findRandoopTests(Path targetDir) {
+    final List<Path> testFiles = Util.findFiles(targetDir, JAVA_MATCHER);
+    if (testFiles.isEmpty()) {
+      return ImmutableSet.of();
+    }
+
+    return testFiles.stream()
+        .map(Path::toFile)
+        .filter(file -> RANDOOP_TEST_NAME_REGEX.asPredicate().test(file.getName()))
+        .map(File::toPath)
+        .collect(ImmutableSet.toImmutableSet());
+  }
+
+  /**
+   * List all Java files found in a directory. Skip those files matching the provided skip hints.
+   *
+   * @param directory   directory to walk into.
+   * @param pathMatcher strategy for match operations on paths
+   * @param skipHints   hints for files to be excluded in the directory.
+   * @return the list of files matching a given extension.
+   */
+  public static List<Path> findFiles(Path directory, PathMatcher pathMatcher, String... skipHints) {
+    if (!Files.isDirectory(directory) || !Files.exists(directory)) {
+      return ImmutableList.of();
+    }
+
+    final Set<String> skipUniverse = Arrays.stream(skipHints)
+        .filter(Objects::nonNull)
+        .collect(ImmutableSet.toImmutableSet());
+
+    final List<Path> results = new ArrayList<>();
+    try (Stream<Path> walk = Files.walk(directory)) {
+      walk.filter(Files::isReadable)
+          .filter(Files::isRegularFile)
+          .filter(file -> pathMatcher.matches(file.getFileName()))
+          .filter(file -> !Util.anyMatches(file, skipUniverse))
+          .forEach(results::add);
+    } catch (IOException ignored) {
+    }
+
+    return results;
+  }
+
+  public static Optional<Path> findRandoopJar(String seedPath, String randoopVersion) {
+    Optional<Path> parentDir = findParentDir(seedPath, "repository");
+
+    Path repository;
+    if (!parentDir.isPresent()) {
+      final Path orElse = Paths.get(
+          System.getProperty("user.home"),
+          ".m2",
+          "repository");
+
+      if (!Files.exists(orElse)) {
+        return Optional.empty();
+      }
+      repository = orElse;
+    } else {
+      repository = parentDir.get();
+    }
+
+    final Path randoopJar = repository.resolve("randoop")
+        .resolve("randoop-all")
+        .resolve(randoopVersion)
+        .resolve("randoop-all-" + randoopVersion + ".jar");
+
+    if (!Files.exists(randoopJar)) {
+      return Optional.empty();
+    }
+
+    return Optional.of(randoopJar);
+  }
+
+  public static Optional<Path> findParentDir(String filepath, String targetName) {
+    // Only valid dir names allowed
+    Preconditions.checkArgument(!Strings.isNullOrEmpty(targetName));
+
+    Path current = Paths.get(filepath);
+    if (!Files.exists(current)) {
+      return Optional.empty();
+    }
+
+    while (current != null && !current.getFileName().toString().equals(targetName)) {
+      current = current.getParent();
+    }
+
+    return Optional.ofNullable(current);
+  }
+
+  public static Set<Class<?>> classesLookup(final String packageName, ClassLoader loader)
+      throws IOException {
+    Preconditions.checkNotNull(packageName);
+
+    final ClassLoader lenientClassLoader = Optional
+        .ofNullable(loader)
+        .orElse(ClassLoader.getSystemClassLoader());
+
+    return ClassPath.from(lenientClassLoader)
+        .getAllClasses()
+        .stream()
+        .filter(cls -> cls.getPackageName().equalsIgnoreCase(packageName))
+        .map(ClassInfo::load)
+        .collect(Collectors.toSet());
+  }
+
+
+  public static void deleteDir(Path path) {
+    try {
+      Files.delete(path);
+    } catch (IOException ignored) {
+    }
+  }
+
+  static boolean anyMatches(Path file, Set<String> skipUniverse) {
+    return skipUniverse.stream().anyMatch(entry -> file.getFileName().toString().contains(entry));
+  }
+
+  public static URL newURL(final String filepath) throws MalformedURLException {
+    return Paths.get(filepath).toUri().toURL();
+  }
+
+  public static URL[] toURLArray(final Collection<URL> candidateURLs){
+    return candidateURLs.toArray(new URL[0]);
+  }
+}
