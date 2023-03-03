2995887f09c064aefd22868a58ff537c2702e058
Huascar <huascar.sanchez@gmail.com>
2023-03-03 13:49:49 -0800

    changes lifecycle phase of the soundness plugin

--- src/main/java/randoop/maven/Soundness.java
+++ src/main/java/randoop/maven/Soundness.java
@@ -22,13 +22,14 @@ import java.util.stream.Collectors;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import randoop.maven.utils.Util;
 
 @SuppressWarnings("unused")
-@Mojo(name = "test-soundness")
+@Mojo(name = "test-soundness", defaultPhase = LifecyclePhase.POST_INTEGRATION_TEST)
 public class Soundness extends AbstractMojo {
   @Parameter(required = true)
   private String packageName;
